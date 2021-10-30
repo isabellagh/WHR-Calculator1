adf3434e7da7b23a8df482fa6a320b9d300afaf0
isabellagh <isabellagh88@gmail.com>
2021-10-30 13:12:07 -0400

    Circ component created

---
+++ src/components/Circ.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Circ = () => {
+  return (
+    <div>
+      <br />
+      <imput>Waist circ</imput>
+      <br />
+      <imput>Hip circ</imput>
+      <br />
+      <br />
+      <button>Calculate</button>
+    </div>
+  );
+};
+
+export default Circ;
--- src/components/WHRCalculator.js
+++ src/components/WHRCalculator.js
@@ -1,27 +1,22 @@
 import React from "react";
-import { WHRContainer, WHRHeader, WHRWrapper, } from "./WHRElements";
+import { WHRContainer, WHRHeader, WHRWrapper } from "./WHRElements";
 import Gender from "./Gender";
+import Circ from "./Circ";
 
 function WHRCalculator() {
-
   return (
     <>
-    <WHRContainer>
-      <WHRHeader>WHR Calculator</WHRHeader>
-      <WHRWrapper>
-      <h3>Today's date: </h3>
-      <Gender />
-      <br />
-      <imput>Waist circ</imput>
-      <imput>Hip circ</imput>
-      <br />
-      <button>Calculate</button>
-      <br />
-      <p>Here is your result:</p>
-      </WHRWrapper>
-    </WHRContainer>
+      <WHRContainer>
+        <WHRHeader>WHR Calculator</WHRHeader>
+        <WHRWrapper>
+          <h3>Today's date: </h3>
+          <Gender />
+          <Circ />
+          <p>Here is your result:</p>
+        </WHRWrapper>
+      </WHRContainer>
     </>
   );
-};
+}
 
 export default WHRCalculator;
